a616281e7169df7d0430e563087178610e8afa00
Florian Ziesche <florian.ziesche@gmail.com>
2024-03-05 23:14:59 +0100

    * made all bug report URLs point to this repo

--- utils/bazel/llvm-project-overlay/llvm/include/llvm/Config/config.h
+++ utils/bazel/llvm-project-overlay/llvm/include/llvm/Config/config.h
@@ -24,7 +24,7 @@
 #include "llvm/Config/llvm-config.h"
 
 /* Bug report URL. */
-#define BUG_REPORT_URL "https://github.com/llvm/llvm-project/issues/"
+#define BUG_REPORT_URL "https://github.com/a2flo/floor_llvm/issues/"
 
 /* Define to 1 to enable backtraces, and to 0 otherwise. */
 #define ENABLE_BACKTRACES 1
@@ -332,7 +332,7 @@
 /* LTDL_SHLIB_EXT defined in Bazel */
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "https://github.com/llvm/llvm-project/issues/"
+#define PACKAGE_BUGREPORT "https://github.com/a2flo/floor_llvm/issues/"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "LLVM"
